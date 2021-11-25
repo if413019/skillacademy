9275eab2548122a4195d74b71a5ff3253a14006d
Yohanes Hutabarat <yohanes.hutabarat@rupiahcepat.co.id>
2021-11-25 21:34:53 +0700

    add locator variables

---
+++ Resources/PageObjects/SkillAcademy/Locators/Locators.py
@@ -0,0 +1,25 @@
+# Navigation Bar locators
+SkillAcademyLogo = "//img[@alt='Skill Academy Logo']"
+EnterpriseLink = "//a[@data-testid='header-enterprise-skillacademy-button']/p"
+PrakerjaLink = "//a[@href='/prakerja']/p"
+KategoriDropdown = "//div[@data-testid='dropdown-category']/p"
+SearchInput = "//input[@data-testid='search-input-field']"
+SearchButton = "//div[@data-testid='search-icon']"
+ReferralLink = "//a[@href='/referral/detail?scrollTo=&activeTab=']"
+SearchButton = "//div[@data-testid='header-login-button']"
+
+# Home Page locators
+MenuCategories = "//div[@data-testid='menu-category']"
+
+# Search Page Locators
+TotalResult = "//span[@data-testid='total-result']"
+CurrentKeyword = "//span[@data-testid='current-keyword']"
+FilterHarga = "//p[text()='Filter']/following-sibling::div/div[@data-testid='dropdown-container'][1]"
+FilterDurasi = "//p[text()='Filter']/following-sibling::div/div[@data-testid='dropdown-container'][2]"
+FilterDurasi = "//p[text()='Urutkan']/following-sibling::div/div[@data-testid='dropdown-container']"
+CourseCards = "//div[contains(@data-testid, 'course-card')]"
+CourseTitles = "//h2[@data-testid='course-title']"
+PaginationPerPage = "//div[@data-testid='pagination-per-page']"
+TotalItem = "//p[@data-testid='total-item']"
+PaginationPrev = "//div[@data-testid='pagination-prev']"
+PaginationNext = "//div[@data-testid='pagination-next']"
